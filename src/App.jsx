29cf26b78af50824c453d1cefe43647807cf4610
Tiago Vidao <@tiagowilliam47@gmail.com>
2025-08-20 17:10:02 -0300

    Resolve merge conflict in src/App.jsx  keep About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import './App.css';
-<<<<<<< HEAD
 import About from './sections/About/About';
-=======
->>>>>>> 26dfeb38730f3ca6b272dd1716250b223230d24d
 import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
@@ -13,10 +10,7 @@ function App() {
   return (
     <>
       <Hero />
-<<<<<<< HEAD
       <About />
-=======
->>>>>>> 26dfeb38730f3ca6b272dd1716250b223230d24d
       <Projects />
       <Skills />
       <Contact />
@@ -25,8 +19,4 @@ function App() {
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> 26dfeb38730f3ca6b272dd1716250b223230d24d
